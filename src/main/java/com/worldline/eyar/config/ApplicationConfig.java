4ff777cbd34a8c259eecea433db92ffaf50b460c
ehsanyar1991 <1240035675>
2021-07-10 00:28:51 +0430

    Config security jpa auditing

--- src/main/java/com/worldline/eyar/config/ApplicationConfig.java
+++ src/main/java/com/worldline/eyar/config/ApplicationConfig.java
@@ -1,18 +1,27 @@
 package com.worldline.eyar.config;
 
+import com.worldline.eyar.config.security.SecurityAuditorAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class ApplicationConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new SecurityAuditorAware();
     }
 
 }
---
+++ src/main/java/com/worldline/eyar/config/security/SecurityAuditorAware.java
@@ -0,0 +1,17 @@
+package com.worldline.eyar.config.security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.ofNullable(authentication != null ? authentication.getName() : null);
+    }
+
+}
